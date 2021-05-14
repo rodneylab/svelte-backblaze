f8dc41ba57b371002f4ba93da83b0324c48f2e87
Rodney Lab <codesign@rodneylab.com>
2021-05-14 17:55:35 +0100

    feat: ✨ added netlify build support

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
+		adapter: adapter(),
 		// amp: true,
 		floc: false,
 		// hydrate the <div id="svelte"> element in src/app.html
