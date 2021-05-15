c138be4ec21df58dd9b4182bf738b6177d93429f
Rodney Lab <codesign@rodneylab.com>
2021-05-15 08:34:03 +0100

    fix: 💫 updated layout footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,10 +9,40 @@
 </main>
 <footer>
 	<p>
-		A project by <a
-			aria-label="Open the Rodney Lab site"
-			href="https://rodneylab.com/"
-			rel="noopener">RODNEY LAB</a
+		Copyright {'\u00A9'} 2021 Rodney Johnson.
+	</p>
+	<ul>
+		<li>
+			<a
+				aria-label="Go to the Rodney Lab Facebook page"
+				href={`https://www.facebook.com/${import.meta.env.VITE_FACEBOOK_PAGE}`}>f</a
+			>
+		</li>
+		<li>
+			<a
+				aria-label="Open the Rodney Lab twitter feed"
+				href={`https://twitter.com/intent/user?screen_name=${
+					import.meta.env.VITE_TWITTER_USERNAME
+				}`}>t</a
+			>
+		</li>
+		<li>
+			<a
+				aria-label="Open the Rodney Lab LinkedIn page"
+				href={`https://uk.linkedin.com/in/${import.meta.env.VITE_LINKED_PROFILE}`}>l</a
+			>
+		</li>
+		<li>
+			<a
+				aria-label="Open the Rodney Lab Git Hub repo"
+				href={`https://github.com/${import.meta.env.VITE_GITHUB_PAGE}`}>g</a
+			>
+		</li>
+	</ul>
+	<p>
+		A project by
+		<a aria-label="Open the Rodney Lab site" href="https://rodneylab.com/" rel="noopener"
+			>RODNEY LAB</a
 		>.
 	</p>
 </footer>
