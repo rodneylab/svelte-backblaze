274be8a540176a74537a7890bada20436f81bafe
Rodney Lab <codesign@rodneylab.com>
2021-05-15 11:06:47 +0100

    style: 🛁 linted login page

--- src/routes/dashboard/login.svelte
+++ src/routes/dashboard/login.svelte
@@ -32,15 +32,15 @@
 </form>
 
 <style>
-  .screen-reader-text {
-    border: 0;
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    height: 1px;
-    margin: -1px;
-    width: 1px;
-    overflow: hidden;
-    position: absolute !important;
-    word-wrap: normal !important;
-  }
+	.screen-reader-text {
+		border: 0;
+		clip: rect(1px, 1px, 1px, 1px);
+		clip-path: inset(50%);
+		height: 1px;
+		margin: -1px;
+		width: 1px;
+		overflow: hidden;
+		position: absolute !important;
+		word-wrap: normal !important;
+	}
 </style>
